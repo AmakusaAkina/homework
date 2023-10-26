53ebf3b83a25de390640f2fd7ef557a565f880d0
AmakusaHaruka <149086335+AmakusaAkina@users.noreply.github.com>
2023-10-26 22:58:49 +0800

    Add files via upload
    第三单元例题

---
+++ the four fundamental operations of arithmetic.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    float num1,num2;
+    char op;
+    scanf("%f%c%f",&num1,&op,&num2);
+    float result;
+    switch(op)
+    {
+        case '+':
+            result=num1+num2;
+            break;
+        case '-':
+            result=num1-num2;
+            break;
+        case '*':
+            result=num1*num2;
+            break;
+        case '/':
+            result=num1/num2;
+            break;
+        default:
+            printf("Invalid operator");
+            break;
+    }
+    //printf("%f",result);
+    printf("%.2f",result);
+    system("pause");
+    return 0;
+}
